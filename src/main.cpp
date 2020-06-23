6d10249abda3ec701c6e62f52541f9a703602dbf
Paritosh Sharma <paritosh.sharmas@gmail.com>
2020-06-23 22:23:34 +0200

    Added Linelinelist

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,32 @@ Qt3DCore::QEntity* addSkybox()
     return skybox;
 }
 
-Qt3DCore::QEntity *createScene()
+void fillLineList(std::shared_ptr<LineList> lineList, int j)
+{
+    for(int i = 1; i < 10; i++)
+    {
+        dms *ra = new dms(10*i);
+        dms *dec = new dms(10*j);
+        std::shared_ptr<SkyPoint>  point(new SkyPoint(*ra, *dec));
+        lineList->append(point);
+    }    
+}
+
+std::shared_ptr<LineListList> fillLineListList()
+{
+    std::shared_ptr<LineListList> lineListList(new LineListList);
+
+    for(int i = 1; i < 10; i++)
+    {
+        std::shared_ptr<LineList> lineList(new LineList);
+        fillLineList(lineList, i);
+        lineListList->append(lineList);
+    }
+
+    return lineListList;
+}
+
+Qt3DCore::QEntity *addLine(SkyPoint *pLast, SkyPoint *pThis)
 {
     // Root entity
     Qt3DCore::QEntity *lineEntity = new Qt3DCore::QEntity;
@@ -51,14 +76,8 @@ Qt3DCore::QEntity *createScene()
     QMatrix4x4 instTransform = QMatrix4x4(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
 
     QVector<QVector3D> pos;
-    for(int i = 1; i <= 2; i++)
-    {
-        dms *ra = new dms(i*36);
-        dms *dec = new dms(i*30);
-        std::shared_ptr<SkyPoint>  point(new SkyPoint(*ra, *dec));
-
-        pos.append(QVector3D(point->ra().radians(), point->dec().radians(), 0));
-    }
+    pos.append(QVector3D(pLast->ra().radians(), pLast->dec().radians(), 0));
+    pos.append(QVector3D(pThis->ra().radians(), pThis->dec().radians(), 0));
     qDebug()<<pos;
 
     // Line Geometry
@@ -87,6 +106,56 @@ Qt3DCore::QEntity *createScene()
     return lineEntity;
 }
 
+Qt3DCore::QEntity* addSkyPolyline(LineList *lineList)
+{
+    Qt3DCore::QEntity* skyPolyLine = new Qt3DCore::QEntity();
+
+    SkyList *points = lineList->points();
+
+    for (int j = 1; j < points->size(); j++)
+    {
+        SkyPoint *pLast = points->at(j++).get();
+        SkyPoint *pThis = points->at(j).get();
+        Qt3DCore::QEntity* line = addLine(pLast, pThis);
+        line->setParent(skyPolyLine);
+    }
+
+    return skyPolyLine;
+}
+
+Qt3DCore::QEntity* addLines()
+{
+    // Add lines on the celestial sphere
+    Qt3DCore::QEntity* lines = new Qt3DCore::QEntity();
+
+    std::shared_ptr<LineListList> lineListList = fillLineListList();
+
+    for (int i = 0; i < lineListList->size(); i++)
+    {
+        std::shared_ptr<LineList> lineList = lineListList->at(i);
+
+        Qt3DCore::QEntity* line = addSkyPolyline(lineList.get());
+        line->setParent(lines);
+    }
+    
+    return lines;
+}
+
+Qt3DCore::QEntity* createTestScene()
+{
+    Qt3DCore::QEntity *root = new Qt3DCore::QEntity;
+
+    // add skybox
+    Qt3DCore::QEntity *skybox = addSkybox();
+    skybox->setParent(root);
+
+    // add polyline
+    Qt3DCore::QEntity *lines = addLines();
+    lines->setParent(root);
+
+    return root;
+}
+
 int main(int argc, char *argv[])
 {
     // Application
@@ -112,12 +181,8 @@ int main(int argc, char *argv[])
     Qt3DExtras::QFirstPersonCameraController *camController = new Qt3DExtras::QFirstPersonCameraController(rootEntity);
     camController->setCamera(camera);
 
-    Qt3DCore::QEntity *scene = createScene();
+    Qt3DCore::QEntity *scene = createTestScene();
     scene->setParent(rootEntity);
-
-    // add skybox
-    Qt3DCore::QEntity *skyboxEntity = addSkybox();
-    skyboxEntity->setParent(rootEntity);
 
     view->setRootEntity(rootEntity);
     view->show();
